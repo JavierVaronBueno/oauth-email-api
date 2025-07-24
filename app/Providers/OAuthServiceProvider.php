0527fedc2515f676fe9249418e94b485cd8d596a
javier_varon <javoxdaemon@gmail.com>
2025-07-24 11:55:59 -0500

    Fix - Updated code Singleton implementation. Docs - OAuth Service Provider Documentation.

--- app/Providers/OAuthServiceProvider.php
+++ app/Providers/OAuthServiceProvider.php
@@ -9,22 +9,32 @@ use Illuminate\Support\ServiceProvider;
 
 /**
  * Service provider for registering OAuth services.
+ *
+ * This provider is responsible for binding the OAuthServiceInterface
+ * to its concrete implementations within the Laravel service container.
  */
 class OAuthServiceProvider extends ServiceProvider
 {
     /**
-     * Register services.
+     * Register any application services.
+     *
+     * This method is where you can bind interfaces to implementations,
+     * register singletons, and perform other service container registrations.
      *
      * @return void
      */
     public function register()
     {
-        $this->app->bind(OAuthServiceInterface::class, GoogleOAuthService::class);
-        $this->app->bind(OAuthServiceInterface::class, MicrosoftOAuthService::class);
+        $this->app->singleton(GoogleOAuthService::class);
+        $this->app->singleton(MicrosoftOAuthService::class);
     }
 
     /**
-     * Bootstrap services.
+     * Bootstrap any application services.
+     *
+     * This method is called after all other service providers have been registered,
+     * meaning you have access to all other services that have been registered
+     * by the framework and other service providers.
      *
      * @return void
      */
